553d9268d9f36264179817c9bf91a0805c488669
alansastre <alan@alansastre.co>
2021-07-30 13:43:05 +0200

    auth controller

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -11,4 +11,9 @@ import com.example.demo.domain.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	Optional<User> findByUsername(String username);
+	
+	Boolean existsByUsername(String username);
+	
+	Boolean existsByEmail(String email);
+	
 }
