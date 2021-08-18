0d8f227c1ae4632eb5cc7030a9ca6552d26179cf
alansastre <alan@alansastre.co>
2021-08-18 12:17:57 +0200

    swagger authorization access - spring boot jwt

--- src/main/java/com/example/demo/security/config/SecurityConfig.java
+++ src/main/java/com/example/demo/security/config/SecurityConfig.java
@@ -77,8 +77,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .antMatchers("/api/hello/**").permitAll()
-                .antMatchers("/v2/**").permitAll()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated();
 
